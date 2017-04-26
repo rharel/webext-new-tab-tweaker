f630b6251e0b3ef7f44bf3efa866026090612a41
Raoul Harel <raoul.harel@gmail.com>
2017-04-26 23:02:03 +0200

    implement redirect-, color-, and image-tabs

--- options/js/options.js
+++ options/js/options.js
@@ -1,55 +1,33 @@
 (function()
-{   /**
-     * The key to the configuration object in local storage.
-     */
-    const CONFIGURATION_STORAGE_KEY = "configuration";
-
+{
     /**
      * The message to display upon an unsuccessful configuration load.
      */
     const LOAD_ERROR_MESSAGE =
-        "An error occurred while loading your saved settings.";
+        "An error occurred while loading your settings.";
     /**
      * The message to display upon an unsuccessful configuration save.
      */
     const SAVE_ERROR_MESSAGE =
         "An error occurred while saving your settings.";
+	/**
+	 * The message to display upon an unsuccessful validation of user input.
+	 */
+    const VALIDATION_ERROR_MESSAGE =
+		"The selected options are not valid. Please make sure all fields are " +
+		"filled-in correctly.";
 
-    /**
-     * Enumerates possible background types.
-     */
-    const BackgroundType =
-    {   Solid: "solid",
-        Image: "image"
-    };
+    const TabType = CustomNewTab.TabType;
+    const default_options = CustomNewTab.default_options;
+    const load_options = CustomNewTab.load_options_from_storage;
+    const save_options = CustomNewTab.save_options_to_storage;
+	const update_ui = CustomNewTab.OptionsPage.update;
 
-    /**
-     * Creates a default configuration object.
-     *
-     * @returns
-     *      A configuration object with default values of its properties.
-     */
-    function create_default_configuration()
-    {   return {
-            /**
-         * The selected background type.
-         */
-            background_type: BackgroundType.Solid,
-            /**
-             * The selected color for the solid-background type.
-             */
-            solid_color: "#ffffff",
-            /**
-             * The selected image path for the image-background type.
-             */
-            image_url: ""
-        };
-    }
-
-    // These will contain form-elements proceeding a call to initialize().
+    // These will contain DOM elements proceeding a call to initialize().
     let error_message;
-    let solid_bg_radio, solid_color_input;
-    let image_bg_radio, image_url_input;
+    let redirect_tab_radio, redirect_url_input;
+    let color_tab_radio, color_background_input;
+    let image_tab_radio, image_url_input;
 
     /**
      * Displays the specified error message to the user.
@@ -58,123 +36,153 @@
      *      The message to display.
      */
     function display_error(message)
-    {   error_message.innerHTML = message;
+    {
+        error_message.innerHTML = message;
     }
 
     /**
      * Reads an options configuration from the page's controls.
      *
      * @returns
-     *      A configuration object.
+     *      An option configuration object.
      */
-    function read_configuration_from_ui()
-    {   let cfg = create_default_configuration();
+    function read_options_from_ui()
+    {
+    	let options = default_options();
 
-        if (solid_bg_radio.checked)
-        {   cfg.background_type = BackgroundType.Solid;
-            cfg.solid_color = solid_color_input.value;
+    	if (redirect_tab_radio.checked)
+		{
+			options.tab_type = TabType.Redirect;
+			options.redirect_url = redirect_url_input.value;
+		}
+        else if (color_tab_radio.checked)
+        {
+        	options.tab_type = TabType.Color;
+            options.color = color_background_input.value;
         }
-        else if (image_bg_radio.checked)
-        {   cfg.background_type = BackgroundType.Image;
-            cfg.image_url = image_url_input.value;
+        else if (image_tab_radio.checked)
+        {
+        	options.tab_type = TabType.Image;
+            options.image_url = image_url_input.value;
         }
 
-        return cfg;
+        return options;
     }
     /**
      * Applies an options configuration to the page's controls.
      *
-     * @param cfg
-     *      The configuration to apply.
+     * @param options
+     *      The option configuration to apply.
      */
-    function apply_configuration_to_ui(cfg)
-    {   solid_bg_radio.checked =
-            cfg.background_type === BackgroundType.Solid;
-        image_bg_radio.checked =
-            cfg.background_type === BackgroundType.Image;
+    function apply_options_to_ui(options)
+    {
+    	redirect_tab_radio.checked =
+			options.tab_type === TabType.Redirect;
+    	color_tab_radio.checked =
+            options.tab_type === TabType.Color;
+        image_tab_radio.checked =
+            options.tab_type === TabType.Image;
 
-        solid_color_input.value = cfg.solid_color;
-        image_url_input.value = cfg.image_url;
+        redirect_url_input.value = options.redirect_url;
+        color_background_input.value = options.color;
+        image_url_input.value = options.image_url;
 
-        options_ui.update();
+        update_ui();
     }
 
-    /**
-     * Loads an option configuration from local storage asynchronously.
-     *
-     * @returns
-     *      A promise which yields a configuration object when fulfilled.
-     */
-    function load_configuration_from_storage()
-    {   return browser.storage.local
-            .get(CONFIGURATION_STORAGE_KEY)
-            .then(item =>
-            {   const cfg = item[CONFIGURATION_STORAGE_KEY];
-                if (cfg) { return cfg; }
-                else { return create_default_configuration(); }
-            })
-            .catch(() => display_error(LOAD_ERROR_MESSAGE));
-    }
-    /**
-     * Saves an option configuration to local storage asynchronously.
-     *
-     * @param cfg
-     *      The configuration to save.
-     */
-    function save_configuration_to_storage(cfg)
-    {   const item = {};
-        item[CONFIGURATION_STORAGE_KEY] = cfg;
-
-        return browser.storage.local
-            .set(item)
-            .then()
-            .catch(() => display_error(SAVE_ERROR_MESSAGE));
-    }
+	/**
+	 * Validates the specified option configuration.
+	 *
+	 * @param options
+	 * 		The options configuration to validate.
+	 * @returns
+	 * 		True iff the selected option configuration is valid.
+	 */
+	function validate_options(options)
+	{
+		if (options.tab_type === TabType.Redirect)
+		{
+			return options.redirect_url.trim().length > 0;
+		}
+		else if (options.tab_type === TabType.Color)
+		{
+			return /^#(?:[0-9a-f]{6})$/.test(options.color);
+		}
+		else if (options.tab_type === TabType.Image)
+		{
+			return options.image_url.trim().length > 0;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
     /**
      * Resets the active option configuration to its default state.
      */
-    function reset_ui_configuration()
-    {   const cfg = create_default_configuration();
-        apply_configuration_to_ui(cfg);
+    function reset_ui_options()
+    {
+    	const options = default_options();
+        apply_options_to_ui(options);
     }
     /**
      * Saves the selected option configuration to local storage.
      */
-    function save_ui_configuration_to_storage()
-    {   const cfg = read_configuration_from_ui();
-        save_configuration_to_storage(cfg);
+    function save_ui_options()
+    {
+    	const options = read_options_from_ui();
+        if (validate_options(options))
+		{
+			display_error("");
+			save_options(options)
+				.catch(() => display_error(SAVE_ERROR_MESSAGE));
+		}
+		else
+		{
+			display_error(VALIDATION_ERROR_MESSAGE);
+		}
     }
 
     /**
      * Initializes the page.
      */
     function initialize()
-    {   error_message = document.getElementById('error-message');
+    {
+    	error_message = document.getElementById('error-message');
 
-        solid_bg_radio = document.getElementById('solid-bg-radio');
-        solid_color_input = document.getElementById('solid-color-input');
+    	redirect_tab_radio = document.getElementById('redirect-tab-radio');
+    	redirect_url_input = document.getElementById('redirect-url-input');
 
-        image_bg_radio = document.getElementById('image-bg-radio');
+        color_tab_radio = document.getElementById('color-tab-radio');
+        color_background_input = document.getElementById('color-background-input');
+
+        image_tab_radio = document.getElementById('image-tab-radio');
         image_url_input = document.getElementById('image-url-input');
 
-        load_configuration_from_storage()
-            .then(cfg => apply_configuration_to_ui(cfg));
+		load_options().then
+		(
+			// On fulfillment:
+			options => apply_options_to_ui(options),
+			// On rejection:
+			() => display_error(LOAD_ERROR_MESSAGE)
+		);
 
         document
             .getElementById('options-form')
             .addEventListener('submit', event =>
             {
                 event.preventDefault();
-                save_ui_configuration_to_storage();
+                save_ui_options();
             });
         document
             .getElementById('options-form')
             .addEventListener('reset', event =>
             {
                 event.preventDefault();
-                reset_ui_configuration();
+                reset_ui_options();
             });
     }
+
     document.addEventListener('DOMContentLoaded', initialize);
 })();
--- options/js/options_ui.js
+++ options/js/options_ui.js
@@ -1,37 +1,41 @@
 (function()
-{   // These will contain form-elements proceeding a call to initialize().
-    let solid_bg_radio, solid_bg_options;
-    let image_bg_radio, image_bg_options, active_bg_options;
+{
+    // These will contain DOM elements proceeding a call to initialize().
+    let redirect_tab_radio, redirect_tab_panel;
+	let color_tab_radio, color_tab_panel;
+    let image_tab_radio, image_tab_panel;
+	let active_panel;
 
-    /**
-     * Activates the solid color background-type options panel, and deactivates
-     * all others.
-     */
-    function activate_solid_bg_options()
-    {   active_bg_options.style.display = "none";
-        active_bg_options = solid_bg_options;
-        active_bg_options.style.display = "block";
-    }
-    /**
-     * Activates the image background-type options panel, and deactivates
-     * all others.
-     */
-    function activate_image_bg_options()
-    {   active_bg_options.style.display = "none";
-        active_bg_options = image_bg_options;
-        active_bg_options.style.display = "block";
-    }
+	/**
+	 * Activates the specified options panel, and deactivates all others.
+	 *
+	 * @param panel
+	 * 		The panel to activate.
+	 */
+    function activate_options_panel(panel)
+	{
+		active_panel.style.display = "none";
+		active_panel = panel;
+		active_panel.style.display = "block";
+	}
 
     /**
      * Activates the appropriate options panel based on the selected background
      * type.
      */
     function update_active_options_panel()
-    {   if (solid_bg_radio.checked)
-        {   activate_solid_bg_options();
-        }
-        else if (image_bg_radio.checked)
-        {   activate_image_bg_options();
+    {
+    	if (redirect_tab_radio.checked)
+		{
+			activate_options_panel(redirect_tab_panel);
+		}
+    	else if (color_tab_radio.checked)
+    	{
+    		activate_options_panel(color_tab_panel);
+    	}
+        else if (image_tab_radio.checked)
+        {
+        	activate_options_panel(image_tab_panel);
         }
     }
 
@@ -39,17 +43,32 @@
      * Initializes element variables.
      */
     function initialize()
-    {   solid_bg_radio = document.getElementById('solid-bg-radio');
-        image_bg_radio = document.getElementById('image-bg-radio');
+    {
+    	redirect_tab_radio = document.getElementById('redirect-tab-radio');
+    	color_tab_radio = document.getElementById('color-tab-radio');
+        image_tab_radio = document.getElementById('image-tab-radio');
 
-        solid_bg_options = document.getElementById('solid-bg-options');
-        image_bg_options = document.getElementById('image-bg-options');
-        active_bg_options = solid_bg_options;
+        redirect_tab_panel = document.getElementById('redirect-tab-options');
+        color_tab_panel = document.getElementById('color-tab-options');
+        image_tab_panel = document.getElementById('image-tab-options');
 
-        solid_bg_radio.addEventListener('click', update_active_options_panel);
-        image_bg_radio.addEventListener('click', update_active_options_panel);
+        active_panel = color_tab_panel;
 
-        window.options_ui = { update: update_active_options_panel };
+        redirect_tab_radio.addEventListener('click', update_active_options_panel);
+        color_tab_radio.addEventListener('click', update_active_options_panel);
+        image_tab_radio.addEventListener('click', update_active_options_panel);
     }
+
     document.addEventListener('DOMContentLoaded', initialize);
+
+    if (!window.CustomNewTab)
+    {
+    	window.CustomNewTab = {};
+    }
+    if (!window.CustomNewTab.OptionsPage)
+    {
+    	window.CustomNewTab.OptionsPage = {};
+    }
+
+    window.CustomNewTab.OptionsPage.update = update_active_options_panel;
 })();
