48a9e0d94d2ee77d1c9d0390a3406b9784797491
Raoul Harel <raoul.harel@gmail.com>
2017-04-30 13:43:57 +0200

    add configuration symbols export to window

---
+++ src/configuration/index.js
@@ -0,0 +1,24 @@
+if (window !== 'undefined')
+{
+	if (window.CustomNewTab === 'undefined')
+	{
+		window.CustomNewTab = {};
+	}
+	window.CustomNewTab.Configuration =
+	{
+		CURRENT_VERSION: CURRENT_VERSION,
+
+		is_valid_version_object: is_valid_version_object,
+		compare_versions: compare_versions,
+
+		TabBehavior: TabBehavior,
+		ImageSource: ImageSource,
+
+		DEFAULT_REDIRECTION_CONFIGURATION: DEFAULT_REDIRECTION_CONFIGURATION,
+		DEFAULT_CUSTOM_PAGE_CONFIGURATION: DEFAULT_CUSTOM_PAGE_CONFIGURATION,
+		DEFAULT_CONFIGURATION: DEFAULT_CONFIGURATION,
+
+		load: load,
+		save: save
+	};
+}
