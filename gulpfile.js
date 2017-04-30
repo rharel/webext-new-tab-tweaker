0bde02a210e25f6aba7c7ac6b65bb2b5c802e4c8
Raoul Harel <raoul.harel@gmail.com>
2017-04-30 14:19:44 +0200

    add task: 'build configuration.js'

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,27 @@
-const gulp = require('gulp');
-const mocha = require('gulp-mocha');
+const
+	gulp = require('gulp'),
+	mocha = require('gulp-mocha'),
+	replace = require('gulp-replace'),
+	concat = require('gulp-concat'),
+	iife = require('gulp-iife');
 
 gulp.task('test', () =>
 {
 	gulp.src('test/**/*.test.js', {read: false})
 		.pipe(mocha({reporter: 'nyan'}));
+});
+
+gulp.task('build configuration.js', () =>
+{
+	gulp.src
+	([
+		'src/configuration/version.js',
+		'src/configuration/layout.js',
+		'src/configuration/storage.js',
+		'src/configuration/index.js',
+	])
+		.pipe(replace(/\/\/#begin-dev[\s\S]*?\/\/#end-dev/g, ""))
+		.pipe(concat('configuration.js'))
+		.pipe(iife())
+		.pipe(gulp.dest('dist/common/js/'));
 });
