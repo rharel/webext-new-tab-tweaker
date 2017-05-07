82ec2f9b21b07a0de4b39a019ccf4eb4620378fa
Raoul Harel <raoul.harel@gmail.com>
2017-05-07 22:50:49 +0200

    dont use version-dependent storage keys

--- src/configuration/storage.js
+++ src/configuration/storage.js
@@ -4,7 +4,7 @@ const DEFAULT_CONFIGURATION = require('./layout').DEFAULT;
 /**
  * The key to the configuration object in local storage.
  */
-const KEY = `configuration@v${Version.as_string(Version.CURRENT)}`;
+const KEY = "configuration@new-tab-tweaker";
 /**
  * The extension's local storage
  */
