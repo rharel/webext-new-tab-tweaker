92b9342870f3f221fe7de9c8580da5252e1eab8c
Raoul Harel <raoul.harel@gmail.com>
2017-05-09 21:55:05 +0200

    dump node, test with mocha in browser

---
+++ dist/common/js/ntt.js
@@ -0,0 +1,22 @@
+(function() {
+"use strict";
+
+/**
+ * The core layout of the NTT namespace.
+ */
+window.NTT =
+{
+	Ordering: null,
+
+	Configuration:
+	{
+		DEFAULT: null,
+
+		TabBehavior: null,
+		ImageURL: null,
+
+		Version: null,
+		Storage: null
+	}
+};
+}());
