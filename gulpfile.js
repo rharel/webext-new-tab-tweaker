84ee49a4d2da19748bc16e922f0f6bd25d581d5d
Raoul Harel <raoul.harel@gmail.com>
2017-05-09 22:06:55 +0200

    add automatic packaging

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ const
 	concat = require('gulp-concat'),
 	iife = require('gulp-iife'),
 	open = require('gulp-open'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+	zip = require('gulp-zip');
 
 const iife_settings = {prependSemicolon: false};
 
@@ -60,5 +61,14 @@ gulp.task('test', ['build'], () =>
 	return (
 		gulp.src('./test/**/*.test.html')
 			.pipe(open())
+	);
+});
+
+gulp.task('build and pack', ['build'], () =>
+{
+	return (
+		gulp.src('./dist/*')
+			.pipe(zip('new-tab-tweaker.zip'))
+			.pipe(gulp.dest('./'))
 	);
 });
