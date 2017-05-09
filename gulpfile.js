64f00e3640496d618eada99ea469834c14a4244a
Raoul Harel <raoul.harel@gmail.com>
2017-05-09 19:44:18 +0200

    ditch gulp-mocha and webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,2 @@
 const
-	gulp = require('gulp'),
-	mocha = require('gulp-mocha');
-
-gulp.task('test', () =>
-{
-	gulp.src('test/**/*.test.js', {read: false})
-		.pipe(mocha({reporter: 'spec'}));
-});
+	gulp = require('gulp');
