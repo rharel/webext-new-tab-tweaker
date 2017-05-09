63271a8741b6225e69f08db6821e8eebd4a64675
Raoul Harel <raoul.harel@gmail.com>
2017-05-09 21:54:17 +0200

    replace webpack and gulp-mocha with oldschool build and test tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,64 @@
 const
 	gulp = require('gulp'),
+	clean = require('gulp-clean'),
 	concat = require('gulp-concat'),
-	iife = require('gulp-iife');
+	iife = require('gulp-iife'),
+	open = require('gulp-open'),
+    rename = require('gulp-rename');
 
-gulp.task('build basic.js', () =>
+const iife_settings = {prependSemicolon: false};
+
+gulp.task('clean', () =>
+{
+	return (
+		gulp.src('./dist/common/js', {read: false})
+			.pipe(clean())
+	);
+});
+
+gulp.task('build ntt.js', ['clean'], () =>
+{
+	return (
+		gulp.src('./src/js/ntt.js')
+			.pipe(iife(iife_settings))
+			.pipe(gulp.dest('./dist/common/js/'))
+	)
+});
+gulp.task('build basic.js', ['clean'], () =>
+{
+	return (
+		gulp.src('./src/js/basic/ordering.js')
+			.pipe(rename('basic.js'))
+			.pipe(iife(iife_settings))
+			.pipe(gulp.dest('./dist/common/js/'))
+	);
+});
+gulp.task('build configuration.js', ['clean'], () =>
 {
 	const sources =
 	[
-		'./src/basic/ordering.js',
-		'./src/basic/index.js'
+		'./src/js/configuration/version.js',
+		'./src/js/configuration/layout.js',
+		'./src/js/configuration/storage.js'
 	];
-
 	return (
 		gulp.src(sources)
-			.pipe(concat('basic.js'))
-			.pipe(iife())
-			.pipe(gulp.dest('./dist/basic/js/'))
+			.pipe(iife(iife_settings))
+			.pipe(concat('configuration.js'))
+			.pipe(gulp.dest('./dist/common/js/'))
+	);
+});
+gulp.task('build',
+[
+	'build ntt.js',
+	'build basic.js',
+	'build configuration.js'
+], () => {});
+
+gulp.task('test', ['build'], () =>
+{
+	return (
+		gulp.src('./test/**/*.test.html')
+			.pipe(open())
 	);
 });
