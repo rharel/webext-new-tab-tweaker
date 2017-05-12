f7591c7c4b663c63519caa642b97cbc16ca4f778
Raoul Harel <raoul.harel@gmail.com>
2017-05-12 23:43:21 +0200

    add organized definition of NTT.OptionsUI in its own file

---
+++ dist/options_ui/js/ui.js
@@ -0,0 +1,14 @@
+/**
+ * The OptionsUI namespace layout.
+ */
+window.NTT.OptionsUI =
+{
+	/**
+	 * Hosts dialog construction and display methods.
+	 */
+	Dialog: null,
+	/**
+	 * Hosts new tab related options.
+	 */
+	NewTab: null
+};
