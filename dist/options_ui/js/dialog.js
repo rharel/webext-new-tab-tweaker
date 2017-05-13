ddbc5bd2d367431bf603a6efd4a93c7a3c213aee
Raoul Harel <raoul.harel@gmail.com>
2017-05-13 11:33:07 +0200

    improve code style and add missing docstrings

--- dist/options_ui/js/dialog.js
+++ dist/options_ui/js/dialog.js
@@ -1,4 +1,5 @@
-(function() {
+(function()
+{
 	/**
 	 * This will contain DOM elements proceeding a call to initialize().
 	 */
@@ -7,16 +8,33 @@
 		shadow: null,
 		content: null
 	};
+	/**
+	 * Contains the currently active dialog element (or null if no dialog is
+	 * active).
+	 */
 	let active_dialog = null;
 
+	/**
+	 * Displays the specified element as a dialog.
+	 *
+	 * @param dialog
+	 * 		The element to show. It is assumed that it is a child of
+	 * 		'#dialog-content'.
+	 */
 	function show_dialog(dialog)
 	{
+		if (active_dialog !== null)
+		{
+			hide_active_dialog();
+		}
 		DOM.shadow.style.display = "block";
 		DOM.content.style.display = "block";
 		active_dialog = dialog;
 		active_dialog.style.display = "block";
-
 	}
+	/**
+	 * Hides the active dialog.
+	 */
 	function hide_active_dialog()
 	{
 		if (active_dialog !== null)
@@ -35,12 +53,13 @@
 		DOM.content =
 			document.getElementById('dialog-content');
 
+		// Close the dialog if the user clicks on anything but the dialog
+		// content.
 		DOM.shadow.addEventListener('click', hide_active_dialog);
 
 		// Clicking the shadow closes the dialog, so don't allow clicks on the
 		// content to bubble up to the shadow.
-		DOM.content
-			.addEventListener('click', event => event.stopPropagation());
+		DOM.content.addEventListener('click', event => event.stopPropagation());
 
 		window.NTT.OptionsUI.Dialog =
 		{
