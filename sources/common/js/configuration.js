923672027093c55c3561be3fac1af146fd7be6e1
Raoul Harel <raoul.harel@gmail.com>
2017-08-26 21:35:47 +0200

    bump version to 1.6.0b5

--- sources/common/js/configuration.js
+++ sources/common/js/configuration.js
@@ -64,7 +64,7 @@
 
         configuration.version =
         {
-            CURRENT: create(1, 6, 0, 4),
+            CURRENT: create(1, 6, 0, 5),
             HAS_RELEASE_NOTES: false,
 
             create:    create,
