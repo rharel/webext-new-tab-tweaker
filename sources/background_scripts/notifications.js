e52d4ed42a69a238967793a57cfa212297b7a74a
Raoul Harel <raoul.harel@gmail.com>
2017-08-31 13:37:30 +0200

    introduce a lower bound on notification duration and base reading speed on existing literature

--- sources/background_scripts/notifications.js
+++ sources/background_scripts/notifications.js
@@ -1,11 +1,28 @@
 (function()
 {
-    const active_ids    = [];
-    const reading_speed = 55;  // in ms per character
+    // The minimum amount of time (in ms) a notification remains visible (assuming it has not been
+    // overridden by another with the same identifier).
+    const minimum_visibility_duration = 1000;
+    const reading_speed = (function()  // in ms per character
+    {
+        // The average reading rate in English is ~863 +- 234 characters per minute [1].
+        // To accommodate non-native speakers we shall assume the lower bound.
+        //
+        // 1. https://en.wikipedia.org/wiki/Words_per_minute#Reading_and_comprehension
+        const cpm  = 863 - 234;
+        return 60000 / cpm;
+    })();
+    // Computes the number of ms it takes to read a given text.
+    function compute_reading_time(text)
+    {
+        const length_based_duration = text.length * reading_speed;
+        return Math.max(length_based_duration, minimum_visibility_duration);
+    }
 
     const addon_name = browser.runtime.getManifest().name;
     const icon_url   = browser.extension.getURL("/icons/main_64.png");
-    
+    const active_ids = [];
+
     // Displays a notification message with given identifier. If an earlier notification with same
     // identifier exists, it is discarded. Notifications last a variable duration based on the
     // length of their message text.
@@ -25,7 +42,7 @@
         {
             // Clear the first (oldest) notification
             browser.notifications.clear(active_ids.shift());
-        }, message.length * reading_speed);
+        }, compute_reading_time(message));
     }
     
     define({ notify: notify });
