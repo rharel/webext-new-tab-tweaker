d93a650af553f6e3a763212900ffab6a7952098e
Raoul Harel <raoul.harel@gmail.com>
2017-08-31 13:05:31 +0200

    review formatting and documentation comments for notifications module

--- sources/background_scripts/notifications.js
+++ sources/background_scripts/notifications.js
@@ -1,26 +1,26 @@
-(function() {
-    const active_ids = [];
-    const reading_speed = 55;
+(function()
+{
+    const active_ids    = [];
+    const reading_speed = 55;  // in ms per character
 
     const addon_name = browser.runtime.getManifest().name;
-    const icon_url = browser.extension.getURL("/icons/main_64.png");
+    const icon_url   = browser.extension.getURL("/icons/main_64.png");
     
-    // Display a notification
-    // Close existing message if a message with the same identifier already exists
-    // The notification is closed after a variable delay.
-    function notify(id, message){
-        // Do not display the same message twice by using a active_id
+    // Displays a notification message with given identifier. If an earlier notification with same
+    // identifier exists, it is discarded. Notifications last a variable duration based on the
+    // length of their message text.
+    function notify(id, message)
+    {
+        // This discards any active notifications with the same identifier.
         browser.notifications.create(id,
         {
-            type: "basic",
+            type:    "basic",
             iconUrl: icon_url,
-            title: addon_name,
+            title:   addon_name,
             message: message
         });
 
-        // Add active_id to active_ids, so we can clear it later
         active_ids.push(id);
-
         setTimeout(function()
         {
             // Clear the first (oldest) notification
