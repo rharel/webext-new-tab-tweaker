69877b593d204aaaeffb7bf1fc5408c2308e745f
Raoul Harel <raoul.harel@gmail.com>
2017-09-02 20:21:50 +0200

    move runtime event hookups to main module

--- sources/background_scripts/main.js
+++ sources/background_scripts/main.js
@@ -5,8 +5,36 @@ requirejs.config(
         "common": "common/js",
     }
 });
-requirejs(
-[
-    "common/configuration",
-    "background_scripts/image_context_menu"
-]);
+requirejs(["background_scripts/image_context_menu"]);
+
+browser.runtime.onInstalled.addListener(details =>
+{
+    requirejs(["common/configuration"], configuration =>
+    {
+        const storage = configuration.storage;
+
+        if (details.reason === "install")
+        {
+            storage.save(configuration.create_default());
+        }
+        else if (details.reason === "update")
+        {
+            storage.load().then(options =>
+            {
+                const version  = configuration.version;
+
+                if (version.HAS_RELEASE_NOTES && options.notification.new_features)
+                {
+                    const v = version.CURRENT;
+                    browser.tabs.create({
+                        url: "https://rharel.github.io/webext-new-tab-tweaker/release-notes/" +
+                             `${v.major}-${v.minor}/` +
+                             `${v.major}-${v.minor}-${v.patch}${v.beta > 0 ? `b${v.beta}` : ""}.html`,
+                        active: true
+                    });
+                }
+                storage.save(configuration.update(options));
+            });
+        }
+    });
+});
--- sources/common/js/configuration.js
+++ sources/common/js/configuration.js
@@ -231,38 +231,7 @@
             save: save
         };
     }
-    // Runtime event hookups:
-    {
-        const storage = configuration.storage;
-
-        browser.runtime.onInstalled.addListener(details =>
-        {
-            if (details.reason === "install")
-            {
-                storage.save(configuration.create_default());
-            }
-            else if (details.reason === "update")
-            {
-                storage.load().then(options =>
-                {
-                    const version  = configuration.version;
-
-                    if (version.HAS_RELEASE_NOTES && options.notification.new_features)
-                    {
-                        const v = version.CURRENT;
-                        browser.tabs.create({
-                            url: "https://rharel.github.io/webext-new-tab-tweaker/release-notes/" +
-                                 `${v.major}-${v.minor}/` +
-                                 `${v.major}-${v.minor}-${v.patch}${v.beta > 0 ? `b${v.beta}` : ""}.html`,
-                            active: true
-                        });
-                    }
-                    storage.save(configuration.update(options));
-                });
-            }
-        });
-    }
-
+    
     define(["./ordering"],
     function(ordering)
     {
