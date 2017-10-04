e105e68f3e23aab8dbc0fc5146b0d8d7a6e9e3c2
Raoul Harel <raoul.harel@gmail.com>
2017-10-04 12:03:54 +0200

    top sites: break up apply_options() into smaller pieces

--- sources/url_overrides/newtab/js/top_sites.js
+++ sources/url_overrides/newtab/js/top_sites.js
@@ -10,14 +10,7 @@
         list:       null
     };
 
-    // The maximum number of sites to list.
-    const MAXIMUM_LIST_SIZE = 20;
-    // The maximum number of characters in a site's title. The middle part of titles longer than
-    // this is truncated.
-    const MAXIMUM_TITLE_SIZE = 100;
-    /// The truncated part of long titles is replaced by this.
-    const TRUNCATION_SYMBOL = "...";
-
+    // Toggles visibility of the list.
     function toggle_visibility()
     {
         const style = DOM.list.style;
@@ -35,6 +28,56 @@
         }
     }
 
+    // Truncates the specified title.
+    //
+    // The middle part of titles longer than this is truncated and replaced by the specified
+    // symbol.
+    function truncate_title(title, target_length = 100, truncation_symbol = "...")
+    {
+        if (target_length <= 0)                        { return "";                }
+        if (title.length  <= target_length)            { return title;             }
+        if (target_length <= truncation_symbol.length) { return truncation_symbol; }
+
+        // The title's total length post truncation should equal MAXIMUM_TITLE_SIZE.
+        // The truncated title begins with the original's prefix, followed by the
+        // truncation symbol, and ends with the original's suffix.
+
+        const segment_size = (target_length - truncation_symbol.length) / 2;
+        const prefix_end   = Math.ceil(segment_size);
+        const suffix_start = title.length - Math.floor(segment_size);
+
+        return title.substring(0, prefix_end) +
+               truncation_symbol +
+               title.substring(suffix_start, title.length);
+    }
+    // Adds the specified site to the listing.
+    function add_site(site)
+    {
+        // Use the site's URL if there is no meaningful title.
+        let title = (!site.title || site.title.trim() === "") ?
+                      site.url   :  site.title;
+        title = truncate_title(title);  // Truncates if too long.
+
+        const anchor = document.createElement("a");
+        anchor.className   = "link-button";
+        anchor.href        = site.url;
+        anchor.textContent = title;
+
+        const favicon = new Image();
+        favicon.style.visibility = "hidden";
+        favicon.addEventListener("load", () => { favicon.style.visibility = "visible"; });
+        favicon.src = `${anchor.origin}/favicon.ico`;
+        anchor.insertBefore(favicon, anchor.firstChild);
+
+        const container = document.createElement("div");
+        container.className = "button-container";
+        container.appendChild(anchor);
+
+        DOM.list.appendChild(container);
+    }
+
+    // The maximum number of sites to list.
+    const MAXIMUM_LIST_SIZE = 20;
     // Applies the specified options to the page.
     function apply_options(options)
     {
@@ -43,48 +86,11 @@
         DOM.list    = document.createElement("div");
         DOM.list.id = "top-sites-list";
 
-        browser.topSites.get().then(items =>
+        browser.topSites.get().then(sites =>
         {
-            const displayed_item_count = Math.min(items.length, MAXIMUM_LIST_SIZE);
-            for (let i = 0; i < displayed_item_count; ++i)
-            {
-                const item = items[i];
+            const displayed_item_count = Math.min(sites.length, MAXIMUM_LIST_SIZE);
+            for (let i = 0; i < displayed_item_count; ++i) { add_site(sites[i]); }
 
-                // Use the item's URL if there is no meaningful title.
-                let title = (!item.title || item.title.trim() === "") ?
-                              item.url : item.title;
-                // Truncate long titles:
-                if (title.length > MAXIMUM_TITLE_SIZE)
-                {
-                    // The title's total length post truncation should equal MAXIMUM_TITLE_SIZE.
-                    // The truncated title begins with the original's prefix, followed by the
-                    // truncation symbol, and ends with the original's suffix.
-
-                    const segment_size = (MAXIMUM_TITLE_SIZE - TRUNCATION_SYMBOL.length) / 2;
-                    const prefix_end   = Math.ceil(segment_size);
-                    const suffix_start = title.length - Math.floor(segment_size);
-
-                    title = title.substring(0, prefix_end) + TRUNCATION_SYMBOL +
-                            title.substring(suffix_start, title.length);
-                }
-
-                const anchor = document.createElement("a");
-                anchor.className   = "link-button";
-                anchor.href        = item.url;
-                anchor.textContent = title;
-
-                const favicon = new Image();
-                favicon.style.visibility = "hidden";
-                favicon.addEventListener("load", () => { favicon.style.visibility = "visible"; });
-                favicon.src = `${anchor.origin}/favicon.ico`;
-                anchor.insertBefore(favicon, anchor.firstChild);
-
-                const container = document.createElement("div");
-                container.className = "button-container";
-                container.appendChild(anchor);
-
-                DOM.list.appendChild(container);
-            }
             if (options.visibility === Visibility.ShowOnRequest)
             {
                 DOM.list.style.display = "none";
